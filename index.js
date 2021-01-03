010022838b0e9c61e2f9dd0a317affc121d309e2
Dzeko Dev <wgabrielsousam@gmail.com>
2021-01-03 22:05:18 +0000

    sla

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ function formatCep(value){
   return String(value).replace(/\D/g, "").splice(0, 8).replace(/(\d{5})(\d)/, "$1-$2")
 }
 
-console.log(formatCep("654600001273"))
+console.log(formatCep(01311000))
